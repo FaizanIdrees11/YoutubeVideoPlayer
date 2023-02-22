c67f514a8e31989b10af616177aa623b6cc87b53
FaizanIdrees11 <79058353+FaizanIdrees11@users.noreply.github.com>
2023-02-22 16:24:26 +0500

    changes

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import subprocess
 import warnings
 
-
 try:
     from setuptools import Command, find_packages, setup
     setuptools_available = True
